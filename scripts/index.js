1563ce630a766684d15521b07ccce57170faac54
rajib5570 <rajib5570@gmail.com>
2024-02-19 21:29:22 +0600

    modal added

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,47 @@
+ // all variables declarations
 const allSeatsByClass = document.getElementsByClassName('seat');
 const grandPrice = document.getElementById('grand-price');
+const seatCount = document.getElementById("seat-count");
 let ticketCount = 0;
 let isTicketBooked = false;
+const applyBtn = document.getElementById('apply-btn');
+const inputField = document.getElementById('input-field');
+const totalprice = document.getElementById('total-price');
+const telephone =document.getElementById('telephone');
+const nextButton =document.getElementById('nextBtn');
+const couponSection = document.getElementById('coupon-section');
+const discountSection =document.getElementById('discount-section');
+const discountPrice =document.getElementById('discounted-price');
 
 // for loop for each seat
 for (const seat of allSeatsByClass) {
   const seatNo = seat.innerText;
-//   click event for each seat
+
+
+  //   click event for each seat
   seat.addEventListener("click", selectSeat);
 
-//   toggle function
+
   function selectSeat() {
+    
+    //  count the purchase limit of ticket 
     if(ticketCount >= 4){
       alert('You can\'t buy more than 4 tickets at once');
       return;
     }
     ticketCount++;
+     
 
+      //   toggle function each click in the seat
     if (seat.classList.contains("bg-[#F7F8F8]")) {
 
-        // bg change
+
+          // background change to green
       seat.classList.remove("bg-[#F7F8F8]");
       seat.classList.add("bg-[#1DD100]");
 
-    //   create new div,p tag with classname & value
+
+            //   create new div,p tag with neccessary classname & value
         const creatDiv = document.createElement("div");
         creatDiv.setAttribute("class", "flex justify-between del");
         const p1 = document.createElement("p");
@@ -32,42 +50,46 @@ for (const seat of allSeatsByClass) {
         p1.innerText = seatNo;
         p2.innerText = "Economy";
         p3.innerText = parseFloat("550");
-        // pushing p tag into div as child
+
+
+                // pushing p tag into div as child
         creatDiv.appendChild(p1);
         creatDiv.appendChild(p2);
         creatDiv.appendChild(p3);
-        // inserting newly created div tag into specific location
+             
+        
+                  // inserting newly created div tag into specific location
         const insert = document.getElementById('insert');
         insert.insertBefore(creatDiv,null);
 
-        // total seat count
+
+                    // total seat count from 40 seat
       const totalSeat = document.getElementById("total-seat");
       const remainingSeat = parseFloat(totalSeat.innerText) - 1;
       totalSeat.innerText = remainingSeat;
 
 
-        // seat count in calculation section
-      const seatCount = document.getElementById("seat-count");
+                      // seat count in price calculation section
         seatCount.classList.remove('hidden');
         const newCount= parseFloat(seatCount.innerText)+1;
         seatCount.innerText=newCount;
     
       
         
-        // calculation in total price & grand total
+                          // calculation in total price & grand total
       const totalPrice =document.getElementById('total-price');
       const newTotalPrice = parseFloat(totalPrice.innerText)+parseFloat(p3.innerText);
       totalPrice.innerText = newTotalPrice;
         grandPrice.innerText = newTotalPrice;
 
-        if(parseFloat(seatCount.innerText)>0){
-            isTicketBooked = true;
-        }
+        
     } else {
-        // toggle class for remove selection
+      
+// toggle class for remove selection
       seat.classList.add("bg-[#F7F8F8]");
-      // calculation in total price & grand total
-      const totalPrice =document.getElementById('total-price');
+    
+      
+  // reverse calculation in total price & grand total  
     const newTotalPrice = parseFloat(totalPrice.innerText)-550;
     totalPrice.innerText = newTotalPrice;
     grandPrice.innerText = newTotalPrice;
@@ -76,11 +98,14 @@ for (const seat of allSeatsByClass) {
     // remove newly created div from calculation section
       const removeElement = document.querySelector('.del')
       removeElement.remove();
-        // total seat count
-      const totalSeat = document.getElementById("total-seat");
+       
+      
+      // reverse the total seat count number
       const remainingSeat = parseFloat(totalSeat.innerText) + 1;
       totalSeat.innerText = remainingSeat;
-        // seat count in calculation section
+
+
+        // reverse the seat count number in calculation section
       const seatCount = document.getElementById("seat-count");
         const newCount= parseFloat(seatCount.innerText)-1;
         seatCount.innerText=newCount;
@@ -88,45 +113,44 @@ for (const seat of allSeatsByClass) {
             seatCount.classList.add('hidden');
             
         }
-        if(parseFloat(seatCount.innerText)==0){
-            isTicketBooked = false;
-        }
       
     }
   }
 }
 
 
-const applyBtn = document.getElementById('apply-btn');
-const inputField = document.getElementById('input-field');
-const totalprice = document.getElementById('total-price');
 
-inputField.addEventListener('onkeyup',function(){
-  console.log(inputField.value);
-  if(inputField.value ==='NEW15' || inputField.value==='Couple 20'){
-    applyBtn.setAttribute('disabled',false);
-  }else{
-    applyBtn.setAttribute('disabled',true);
-
-  }
-})
-
-
+// function for the apply button in the coupon section
 applyBtn.addEventListener('click',function(){
+
+  // condition for coupon code "Couple 20"
     if(inputField.value==='Couple 20'){
+      couponSection.setAttribute('class','hidden');
+      discountSection.classList.remove('hidden');
+      discountPrice.innerText = parseFloat(totalprice.innerText)*0.2;
     const newGrandprice = parseFloat(totalprice.innerText)-parseFloat(totalprice.innerText)*0.2;
         grandPrice.innerText = newGrandprice;
 
+        //  condition for coupon code "NEW15"
     }else if(inputField.value ==='NEW15'){
+      couponSection.setAttribute('class','hidden');
+      discountSection.classList.remove('hidden');
+      discountPrice.innerText = parseFloat(totalprice.innerText)*0.15;
         const newGrandprice = parseFloat(totalprice.innerText)-parseFloat(totalprice.innerText)*0.15;
         grandPrice.innerText = newGrandprice;
 
     }
+
+    // condition for without any coupon code
     else{
         grandPrice.innerText = parseFloat(totalprice.innerText);
-        // alert('Invalid Coupon Code');
         inputField.value="";
     
     }
 })
+
+// reload function after completing a purchase
+document.getElementById('btn-continue').addEventListener('click',function(){
+  window.location.reload();
+});
 
